e16aa03e2e241ea10ed7f8c2cbe6263deee0c681
AlptugKizilay <alptugkizilay@gmail.com>
2022-08-09 14:43:33 +0300

    find wheter the word is palindromic

---
+++ palindromicWords/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean isPalindromic(String str){
+        int i=0;
+        int j = str.length()-1;
+        while(i<=j){
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    static boolean isPalindromic2(String str){
+        for (int i = 0, j = str.length()-1; i<=j; i++, j-- ){
+            if (str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Word: ");
+        String word = inp.nextLine();
+        System.out.println(isPalindromic2(word));
+
+    }
+}
